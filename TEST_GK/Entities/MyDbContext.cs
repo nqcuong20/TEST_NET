f334d2019e24f7d5d68ff8673f26953ed182f254
Cuong Nguyen <nguyenquoccuongcn20@gmail.com>
2020-12-16 14:41:16 +0700

    add project

---
+++ TEST_GK/Entities/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST_GK.Entities
+{
+    public class MyDbContext : DbContext
+    {
+    }
+}
